21005b1a9ba748bd11bcaed70192310236dd5727
Pavel (Home) <tspa77@gmail.com>
2020-08-28 10:03:07 +0700

    Initial commit

---
+++ app/src/main/java/com/gmail/tspa77/buttons/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.gmail.tspa77.buttons
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.isEnabled = true
+        button1.isActivated = false
+    }
+}
